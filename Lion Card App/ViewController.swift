3b03b528410ed3a2786880dff8faa33201c58920
amandachristy <test@youremail.com>
2017-01-24 10:09:53 -0500

    Initial Commit

---
+++ Lion Card App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lion Card App
+//
+//  Created by Amanda Christy on 1/24/17.
+//  Copyright © 2017 Amanda Christy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
