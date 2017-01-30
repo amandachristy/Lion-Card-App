52582ee7130265ddbf3bc8b643ac48058ddae3a5
amandachristy <test@youremail.com>
2017-01-29 22:55:14 -0500

    New Lion Card Barcode Coode

--- Lion Card App/ViewController.swift
+++ Lion Card App/ViewController.swift
@@ -9,7 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var qrcodeImage: CIImage!
 
+    
+    
+
+    @IBOutlet weak var imgQRCode: UIImageView!
+    @IBOutlet weak var btnAction: UIButton!
+    @IBOutlet weak var textField: UITextField!
+  
+   
+    @IBAction func performButtonAction(sender: AnyObject){
+        if qrcodeImage == nil {
+            if textField.text == "" {
+                
+            
+            
+            let data = textField.text?.data (using: String.Encoding.isoLatin1, allowLossyConversion: false)
+            
+            let filter = CIFilter(name: "CIQRCodeGenerator")
+            
+            filter?.setValue(data, forKey: "inputMessage")
+            filter?.setValue("Q", forKey: "inputCorrectionLevel")
+            
+            qrcodeImage = filter?.outputImage
+            
+            imgQRCode.image = UIImage(ciImage: qrcodeImage)
+            
+            textField.resignFirstResponder()
+            
+            btnAction.setTitle("Clear", for: UIControlState.normal)
+        }
+        else {
+            imgQRCode.image = nil
+            qrcodeImage = nil
+            btnAction.setTitle("Generate", for: UIControlState.normal)
+        }
+            return
+    }
+    }
+    
+
+
+
+    @IBAction func changeImageViewScale(sender: AnyObject){
+        
+    }
+
+
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
