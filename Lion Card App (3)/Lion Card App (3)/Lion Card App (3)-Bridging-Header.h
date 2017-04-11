e17a3518b29a30586af235490e9d978a9fbcb43b
Seth Battis <sethbattis@stmarksschool.org>
2017-04-11 09:05:52 -0400

    Bridging Header (and .gitignore)

---
+++ Lion Card App (3)/Lion Card App (3)/Lion Card App (3)-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Lion Card App (3)-Bridging-Header.h
+//  Lion Card App (3)
+//
+//  Created by Seth Battis on 4/11/17.
+//  Copyright © 2017 Amanda Christy. All rights reserved.
+//
+
+#ifndef Lion_Card_App__3__Bridging_Header_h
+#define Lion_Card_App__3__Bridging_Header_h
+
+#import <CommonCrypto/CommonHMAC.h>
+
+#endif /* Lion_Card_App__3__Bridging_Header_h */
