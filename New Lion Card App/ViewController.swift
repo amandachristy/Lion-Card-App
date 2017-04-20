336d2911461d331f83de2411182f27f6b4a26678
SabinKim <SabinKim@stmarksschool.org>
2017-04-20 15:02:40 -0400

    04.20.17

---
+++ New Lion Card App/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  New Lion Card App
+//
+//  Created by Sabin Kim  on 20/04/2017.
+//  Copyright © 2017 Amanda Christy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var OutputText: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
